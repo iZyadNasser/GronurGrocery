4579d3045ccf9e9a7967656fe9ea73734b56c522
Zyad <zyadhammad531@gmail.com>
2024-09-04 15:21:37 +0300

    made the form continue button composable

--- app/src/main/java/com/example/gronurgrocery/common/components/CustomButton.kt
+++ app/src/main/java/com/example/gronurgrocery/common/components/CustomButton.kt
@@ -1,11 +1,17 @@
 package com.example.gronurgrocery.common.components
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
@@ -22,7 +29,9 @@ fun CustomButton(
     textColor: Color,
     backgroundColor: Color,
     onClick: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    weight: FontWeight = FontWeight.SemiBold,
+    @DrawableRes iconRes: Int? = null
 ) {
     Box(
         contentAlignment = Alignment.Center,
@@ -33,11 +42,20 @@ fun CustomButton(
             .clickable { onClick() }
             .background(backgroundColor)
     ) {
-        Text(
-            text = text,
-            style = MaterialTheme.typography.bodyLarge,
-            fontWeight = FontWeight.SemiBold,
-            color = textColor
-        )
+        Row {
+            if (iconRes != null) {
+                Image(
+                    painter = painterResource(id = iconRes),
+                    contentDescription = null
+                )
+                Spacer(modifier = Modifier.width(12.dp))
+            }
+            Text(
+                text = text,
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = weight,
+                color = textColor
+            )
+        }
     }
 }
---
+++ app/src/main/java/com/example/gronurgrocery/features/auth/presentation/components/FormContinueWithButton.kt
@@ -0,0 +1,41 @@
+package com.example.gronurgrocery.features.auth.presentation.components
+
+import androidx.annotation.DrawableRes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.gronurgrocery.R
+import com.example.gronurgrocery.common.components.CustomButton
+import com.example.gronurgrocery.features.ui.theme.GronurGroceryTheme
+
+@Composable
+fun FormContinueWithButton(
+    text: String,
+    @DrawableRes iconRes: Int,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    CustomButton(
+        text = text,
+        textColor = Color(0xFF96A4B2),
+        backgroundColor = Color(0xFFF8F8F8),
+        iconRes = iconRes,
+        weight = FontWeight.Normal,
+        onClick = { onClick() },
+        modifier = modifier
+    )
+}
+
+@Preview
+@Composable
+private fun PreviewFormContinueWithButton() {
+    GronurGroceryTheme {
+        FormContinueWithButton(
+            text = "Continue with Google",
+            iconRes = R.drawable.google,
+            onClick = {}
+        )
+    }
+}
