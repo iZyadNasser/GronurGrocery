80d052986a629685c3366cbad7d3a224de33e424
Zyad <zyadhammad531@gmail.com>
2024-09-13 20:21:30 +0300

    removed the default click indicator from the checkbox composable

--- app/src/main/java/com/example/gronurgrocery/features/auth/presentation/common/components/CustomCheckbox.kt
+++ app/src/main/java/com/example/gronurgrocery/features/auth/presentation/common/components/CustomCheckbox.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,6 +12,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -35,7 +37,10 @@ fun CustomCheckbox(
             .border(BorderStroke(1.dp, activeColor), shape = RoundedCornerShape(5.dp))
             .size(20.dp)
             .background(if (!checked) Color.Transparent else background)
-            .clickable { onClick() }
+            .clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() }
+            ) { onClick() }
     ) {
         if (checked) {
             Icon(
